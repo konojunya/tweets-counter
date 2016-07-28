ef19867ddfbf5bf629525f342b3a307fa0b2bf0f
konojunya <junya03dance@gmail.com>
2016-07-29 06:37:41 +0900

    flux周り完成

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ var express = require("express"),
 		http = require("http").Server(app),
 		path = require("path"),
 		Twit = require("twit"),
-		settings = require("./settings.js"),
 		PORT = 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-var twitter = new Twit(settings);
+var twitter = new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000,
+});
 
 app.get("/",function(req,res){
 	res.sendfile("views/main.html");
