8fe92657c4d37efadfbb15c7f0729f167ccf4c4a
konojunya <junya03dance@gmail.com>
2016-08-08 19:28:11 +0900

    port 変更

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express"),
 		Twit = require("twit"),
 		ECT = require('ect'),
     ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }),
-		PORT = 3000;
+		PORT = 6969;
 
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
